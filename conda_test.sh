8b07a1f458e9972fe52e6361cd49c9de33d5399e
Nicholas Cain <nicholasc@alleninstitute.org>
2014-12-17 00:22:10 +0000

    update conda on build

--- conda_test.sh
+++ conda_test.sh
@@ -1,7 +1,7 @@
 PYTHONPATH=''
-yes | conda create -n dipde_test --file requirements.txt
+conda create -n dipde_test --file requirements.txt
 source activate dipde_test
 #py.test
 python setup.py test
 source deactivate
-yes | conda remove -n dipde_test --all
+conda remove -n dipde_test --all
