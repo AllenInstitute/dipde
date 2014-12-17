05a1de2fe0a67053a74af226fa36e6d08ea32b61
Nicholas Cain <nicholasc@alleninstitute.org>
2014-12-17 00:44:39 +0000

    set location of conda env

--- conda_test.sh
+++ conda_test.sh
@@ -1,7 +1,7 @@
 PYTHONPATH=''
-conda create -n dipde_test --file requirements.txt --prefix=`pwd`
+conda create --file requirements.txt --prefix=`pwd`/dipde_test
 source activate dipde_test
 #py.test
 python setup.py test
 source deactivate
-conda remove -n dipde_test --all
+conda remove --all --prefix=`pwd`/dipde_test
