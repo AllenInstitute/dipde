3b270700910f2e95ce10ef82f6a911dd3d110f40
Nicholas Cain <nicholasc@alleninstitute.org>
2015-12-10 23:05:34 +0000

    profiling includes an extractor method to get times

--- dipde/profiling/__init__.py
+++ dipde/profiling/__init__.py
@@ -1,3 +1,4 @@
+import re
 from dipde.internals.simulation import Simulation
 import cProfile
 import pstats
@@ -17,3 +18,11 @@ def profile_simulation(simulation_dict, run_dict, sort_stats='cumulative', print
     
     p.strip_dirs().sort_stats(sort_stats).print_stats(print_stats)
     return stream.getvalue()
+
+def extract_value(result, key, file_name, function_name):
+
+    header_line = re.findall("^.*filename:lineno\(function\).*$", result, re.MULTILINE)[0].strip()
+    key_ind = header_line.split().index(key)
+    
+    value_line = re.findall("^.*%s:.*\(%s\).*$" % (file_name, function_name), result, re.MULTILINE)[0].strip()
+    return float(value_line.split()[key_ind])
--- dipde/test/test_profiling.py
+++ dipde/test/test_profiling.py
@@ -1,16 +1,24 @@
-from dipde.profiling import profile_simulation
+import numpy as np
+from dipde.profiling import profile_simulation, extract_value
 from dipde.examples.singlepop import get_simulation
+import re
+
+
 
 def test_profile_singlepop():
     
-    dv = .00005
+    dv = .0001
     update_method = 'approx'
     approx_order = None
     tol = 1e-14
     
-    run_dict = {'dt':.00005, 't0':0, 'tf':.2}
+    run_dict = {'dt':.0001, 't0':0, 'tf':.2}
     simulation = get_simulation(dv=dv, update_method=update_method, approx_order=approx_order, tol=tol)
-    profile_simulation(simulation.to_dict(), run_dict)
+    total_time_list = []
+    profile_result = profile_simulation(simulation.to_dict(), run_dict)
+    run_time = extract_value(profile_result, 'cumtime', 'simulation.py', 'run')
+
+    
     
 if __name__ == "__main__":                      # pragma: no cover
     test_profile_singlepop()                    # pragma: no cover
