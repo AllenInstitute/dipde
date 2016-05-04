1954fbbcef854eef7b060210f457e0940a98a8de
Nicholas Cain <nicholasc@alleninstitute.org>
2016-05-04 23:20:38 +0000

    fixed coverage report

--- dipde/test/test_basic.py
+++ dipde/test/test_basic.py
@@ -5,3 +5,34 @@ def test_imports():
     import dipde as dd
     reload(dd)
     
+def test_internal():
+    
+    import dipde.internals as dd
+    reload(dd)
+    
+    import dipde.internals.connection as cc
+    reload(cc)
+    
+    import dipde.internals.connectiondistribution as cd
+    reload(cd)
+    
+    import dipde.internals.connectiondistributioncollection as cdc
+    reload(cdc)
+    
+    import dipde.internals.externalpopulation as ep
+    reload(ep)
+    
+    import dipde.internals.internalpopulation as ip
+    reload(ip)
+    
+    import dipde.internals.simulation as ss
+    reload(ss)
+    
+    import dipde.internals.utilities as ut
+    reload(ut)
+    
+
+    
+if __name__ == "__main__":                         # pragma: no cover
+    test_internal()                         # pragma: no cover
+    test_imports()                         # pragma: no cover
