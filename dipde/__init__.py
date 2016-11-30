ad628518ab4723108e0d957c7c94125b6ca06a5a
nicholasc <nicholasc@alleninstitute.org>
2016-11-30 04:10:20 +0000

    updated version number to 0.2.1

--- dipde/__init__.py
+++ dipde/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with dipde.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
