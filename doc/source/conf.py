cf3e751475a8a1c2fb19696eb90e7b8331aaae54
John Yearsley <yearsj@uw.edu>
2017-07-14 15:57:56 -0700

    debug sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,12 +25,12 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.6'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','numpydoc','sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary','numpydoc']
 numpydoc_show_class_members = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,8 +38,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
