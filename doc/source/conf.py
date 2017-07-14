818e5a7c4114a3f07282ec38884f2576f7108a8c
John Yearsley <yearsj@uw.edu>
2017-07-14 16:00:07 -0700

    debug sphinx2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,8 +30,8 @@ needs_sphinx = '1.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary','numpydoc']
-numpydoc_show_class_members = False
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary']
+#numpydoc_show_class_members = False
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
